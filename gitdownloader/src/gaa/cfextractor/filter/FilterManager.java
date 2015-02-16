409c29e92348aaeadc98201f81c2546469b3b7d8
Guilherme Avelino <gaa@dcc.ufmg.br>
2015-02-16 17:29:19 -0200

    Alteração para realizar a filtragem por linguagem, independentemente umas das outras.

--- gitdownloader/src/gaa/cfextractor/filter/FilterManager.java
+++ gitdownloader/src/gaa/cfextractor/filter/FilterManager.java
@@ -9,13 +9,43 @@ import java.util.List;
 public class FilterManager {
 	List<ProjectInfo> projects;
 	List<ProjectFilter> filters;
+	ProjectFilter mainFilter;
+	
 	public static void main(String[] args) throws Exception {
 		FilterManager filterManager =  new FilterManager(new ProjectInfoDAO().findAll(null));
 //		filterManager.addFilter(new NumberOfProjectFilter(filterManager.getProjects(), 500));
-		filterManager.addFilter(new TeamProjectFilter(filterManager.getProjects(), 32));
-		filterManager.addFilter(new HistoryProjectFilter(filterManager.getProjects(), 334));
-		filterManager.addFilter(new SizeProjectFilter(filterManager.getProjects(), 45));
-		filterManager.addFilter(new MigrationProjectFilter(filterManager.getProjects(), 2, 0.5f, 20));
+//		filterManager.addFilter(new TeamProjectFilter(filterManager.getProjects(), 32));
+//		filterManager.addFilter(new HistoryProjectFilter(filterManager.getProjects(), 334));
+//		filterManager.addFilter(new SizeProjectFilter(filterManager.getProjects(), 45));
+//		filterManager.addFilter(new MigrationProjectFilter(filterManager.getProjects(), 2, 0.5f, 20));
+
+		filterManager.addFilter(new TeamProjectFilter(filterManager.getProjectsByLanguage("java"), 17));
+		filterManager.addFilter(new HistoryProjectFilter(filterManager.getProjectsByLanguage("java"), 230));
+		filterManager.addFilter(new SizeProjectFilter(filterManager.getProjectsByLanguage("java"), 100));
+		
+		filterManager.addFilter(new TeamProjectFilter(filterManager.getProjectsByLanguage("javascript"), 40));
+		filterManager.addFilter(new HistoryProjectFilter(filterManager.getProjectsByLanguage("javascript"), 395));
+		filterManager.addFilter(new SizeProjectFilter(filterManager.getProjectsByLanguage("javascript"), 41));
+
+		filterManager.addFilter(new TeamProjectFilter(filterManager.getProjectsByLanguage("php"), 29));
+		filterManager.addFilter(new HistoryProjectFilter(filterManager.getProjectsByLanguage("php"), 380));
+		filterManager.addFilter(new SizeProjectFilter(filterManager.getProjectsByLanguage("php"), 65));
+
+		filterManager.addFilter(new TeamProjectFilter(filterManager.getProjectsByLanguage("python"), 26));
+		filterManager.addFilter(new HistoryProjectFilter(filterManager.getProjectsByLanguage("python"), 295));
+		filterManager.addFilter(new SizeProjectFilter(filterManager.getProjectsByLanguage("python"), 39));
+		
+		filterManager.addFilter(new TeamProjectFilter(filterManager.getProjectsByLanguage("ruby"), 71));
+		filterManager.addFilter(new HistoryProjectFilter(filterManager.getProjectsByLanguage("ruby"), 781));
+		filterManager.addFilter(new SizeProjectFilter(filterManager.getProjectsByLanguage("ruby"), 95));
+		
+		filterManager.addFilter(new TeamProjectFilter(filterManager.getProjectsByLanguage("c/c++"), 23));
+		filterManager.addFilter(new HistoryProjectFilter(filterManager.getProjectsByLanguage("c/c++"), 446));
+		filterManager.addFilter(new SizeProjectFilter(filterManager.getProjectsByLanguage("c/c++"), 130));
+
+		filterManager.setMainFilter(new MigrationProjectFilter(filterManager.getProjects(), 2, 0.5f, 20));
+		filterManager.addFilter(filterManager.getMainFilter());
+		
 		filterManager.cleanAndFilter();
 		filterManager.persistFiltredProjects();
 	}
@@ -45,11 +75,24 @@ public class FilterManager {
 	}
 	
 	public void persistFiltredProjects(){
-		filters.get(0).persistFilterInformations();
+		this.mainFilter.persistFilterInformations();
 	}
 
 	public List<ProjectInfo> getProjects() {
 		return projects;
+	}
+	
+	private List<ProjectInfo> getProjectsByLanguage(String language) {
+		List<ProjectInfo> newProjects = new ArrayList<>();
+		for (ProjectInfo projectInfo : projects) {
+			if (language.equalsIgnoreCase("c/c++")){
+				if (projectInfo.getLanguage().equalsIgnoreCase("c")||projectInfo.getLanguage().equalsIgnoreCase("c++"))
+					newProjects.add(projectInfo);
+			}
+			else if (projectInfo.getLanguage().equalsIgnoreCase(language))
+				newProjects.add(projectInfo);
+		}
+		return newProjects;
 	}
 
 	public void setProjects(List<ProjectInfo> projects) {
@@ -62,6 +105,14 @@ public class FilterManager {
 
 	public void setFilters(List<ProjectFilter> filters) {
 		this.filters = filters;
+	}
+
+	public ProjectFilter getMainFilter() {
+		return mainFilter;
+	}
+
+	public void setMainFilter(ProjectFilter mainFilter) {
+		this.mainFilter = mainFilter;
 	}
 	
 }
